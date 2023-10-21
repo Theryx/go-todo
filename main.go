c929e91f6227c164a6111433ee7fe9cf15062798
Theryx <ndouken@gmail.com>
2023-10-21 03:40:54 +0100

    added database connection and data models

--- main.go
+++ main.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"time"
+
+	"github.com/thedevsaddam/renderer"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var rnd *renderer.Render
+var db *mgo.Database
+
+const (
+	hostName       string = "localhost:27017"
+	dbName         string = "demo_todo"
+	collectionName string = "todo"
+	port           string = ":9000"
+)
+
+type (
+	todoModel struct {
+		ID        bson.ObjectId `bson:"_id,omitempty"`
+		Tittle    string        `bson:"tittle"`
+		Completed bool          `bson:"completed"`
+		CreatedAt time.Time     `bson:"createAt"`
+	}
+	todo struct {
+		ID        string    `json:"id"`
+		Tittle    string    `json:"tittle"`
+		Completed string    `json:"completed"`
+		CreatedAt time.Time `json:"create_at"`
+	}
+)
+
+// This function helps connect with the database and start a new session
+func init() {
+	rnd = renderer.New()
+	sess, err := mgo.Dial(hostName)
+	checkErr(err)
+	sess.SetMode(mgo.Monotonic, true)
+	db = sess.DB(dbName)
+}
